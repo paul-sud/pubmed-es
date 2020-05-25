0ea9ad54ccf19697689ec39e5bb1cc63b2c46058
Paul Sud <paulsud@stanford.edu>
2020-05-25 10:03:56 -0700

    improve parser and add tests

--- pubmed_es/parser.py
+++ pubmed_es/parser.py
@@ -2,7 +2,7 @@ import json
 from collections import OrderedDict
 from gzip import GzipFile
 from pathlib import Path
-from typing import Any, Dict, List
+from typing import Any, Dict, Iterator, List, Tuple
 
 import xmltodict
 
@@ -11,7 +11,9 @@ def get_documents_from_pubmed_xml(pubmed_xml: Path) -> List[Dict[str, Any]]:
     docs = []
 
     def item_callback(path, item):
-        docs.append(item)
+        unordered_item = ordered_dict_to_dict(item)
+        flattened = selectively_flatten_dict(unordered_item)
+        docs.append(flattened)
         return True
 
     xmltodict.parse(
@@ -29,8 +31,46 @@ def get_documents_from_pubmed_xml(pubmed_xml: Path) -> List[Dict[str, Any]]:
         item_callback=item_callback,
     )
 
-    return [ordered_dict_to_dict(i) for i in docs]
+    return docs
 
 
 def ordered_dict_to_dict(data: OrderedDict) -> Dict[str, Any]:
     return json.loads(json.dumps(data))
+
+
+def _selectively_flatten_dict(data: Dict[str, Any]) -> Iterator[Tuple[str, Any]]:
+    for k, v in data.items():
+        if isinstance(v, dict):
+            yield from _selectively_flatten_dict(v)
+        elif isinstance(v, list):
+            new_val = []
+            for item in v:
+                if isinstance(item, dict):
+                    new_val.append({k: v for k, v in _selectively_flatten_dict(item)})
+                else:
+                    new_val.append(item)
+            yield pythonify_key(k), new_val
+        else:
+            yield pythonify_key(k), v
+
+
+def selectively_flatten_dict(data: Dict[str, Any]) -> Dict[str, Any]:
+    return {k: v for k, v in _selectively_flatten_dict(data)}
+
+
+def pythonify_key(key: str) -> str:
+    new_key = ""
+    found_start = False
+    for char in key:
+        if not char.isalnum():
+            continue
+        elif char.isupper():
+            if found_start is False:
+                new_key += char.lower()
+                found_start = True
+            else:
+                new_key += "_" + char.lower()
+        else:
+            new_key += char
+            found_start = True
+    return new_key
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,8 +1,16 @@
 import gzip
+from collections import OrderedDict
 from pathlib import Path
 from unittest.mock import mock_open
 
-from pubmed_es.parser import get_documents_from_pubmed_xml
+import pytest
+
+from pubmed_es.parser import (
+    get_documents_from_pubmed_xml,
+    ordered_dict_to_dict,
+    pythonify_key,
+    selectively_flatten_dict,
+)
 
 article = """
 <PubmedArticleSet>
@@ -61,8 +69,40 @@ def test_get_documents_from_pubmed_xml(mocker):
     pubmed_articles = get_documents_from_pubmed_xml(Path("foo"))
     assert len(pubmed_articles) == 1
     assert (
-        pubmed_articles[0]["PubmedArticle"]["MedlineCitation"]["Article"]["Abstract"][
-            "AbstractText"
-        ]
+        pubmed_articles[0]["abstract_text"]
         == "(--)-alpha-Bisabolol has a primary antipeptic action"
     )
+    assert pubmed_articles[0]["mesh_heading_list"] == [
+        {
+            "major_topic_y_n": "N",
+            "text": "Dose-Response Relationship, Drug",
+            "u_i": "D004305",
+        }
+    ]
+    assert pubmed_articles[0]["chemical_list"] == [
+        {"text": "Hemoglobins", "u_i": "D006454"}
+    ]
+    assert pubmed_articles[0]["pubmed_pub_date"] == "1975-9-1"
+
+
+def test_ordered_dict_to_dict():
+    ordered = OrderedDict()
+    ordered.update({"1": 2, "3": OrderedDict({"foo": "bar"})})
+    result = ordered_dict_to_dict(ordered)
+    assert result == {"1": 2, "3": {"foo": "bar"}}
+
+
+def test_selectively_flatten_dict():
+    data = {
+        "foo": {"Bar": "baz"},
+        "qux": [1, 2, 3],
+        "#spam": [{"eggs": {"quux": "corge"}}],
+    }
+    result = selectively_flatten_dict(data)
+    assert result == {"bar": "baz", "qux": [1, 2, 3], "spam": [{"quux": "corge"}]}
+
+
+@pytest.mark.parametrize("key,expected", [("@foo", "foo"), ("BazQux", "baz_qux")])
+def test_pythonify_key(key, expected):
+    result = pythonify_key(key)
+    assert result == expected
