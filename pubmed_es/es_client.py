93ae5de9d543a35b3bc7580ae33a29c49f3f907c
Paul Sud <paulsud@stanford.edu>
2020-05-31 14:14:09 -0700

    add ES integration test skeleton

--- pubmed_es/es_client.py
+++ pubmed_es/es_client.py
@@ -1,7 +1,7 @@
-from typing import Any, Dict, Optional, List
+from typing import Optional, List
 
 from elasticsearch import Elasticsearch
-from elasticsearch.helpers import parallel_bulk
+from elasticsearch.helpers import parallel_bulk as _parallel_bulk
 
 
 class ElasticsearchClient:
@@ -15,18 +15,9 @@ class ElasticsearchClient:
             self._client = Elasticsearch()
         return self._client
 
-    def create_index(self, index: str, body: Optional[Dict[str, Any]] = None) -> None:
-        """
-        Creates an index in Elasticsearch if one isn't already there.
-        """
-        if body is None:
-            body = {}
-        self.client.indices.create(index=index, body=body, ignore=400)
-        self.indices.append(index)
-
     def parallel_bulk(self, *args, **kwargs):
         """
         See https://elasticsearch-py.readthedocs.io/en/master/helpers.html#elasticsearch.helpers.parallel_bulk
         for details, this is just a wrapper method to unify the helper method and client.
         """
-        parallel_bulk(self.client, *args, **kwargs)
+        _parallel_bulk(self.client, *args, **kwargs)
--- tests/test_es_client.py
+++ tests/test_es_client.py
@@ -1,45 +1,23 @@
-import subprocess
-from time import sleep
-
-from elasticsearch import Elasticsearch
 import pytest
 
-
-@pytest.fixture(scope="session")
-def server_process(tmp_path):
-    args = [
-        "elasticsearch",
-        "-Enetwork.host=127.0.0.1",
-        "-Ehttp.port=9201",
-        f"-Epath.data={tmp_path / 'data'}",
-        f"-Epath.logs={tmp_path / 'logs'}",
-    ]
-    process = subprocess.run(
-        args, close_fds=True, stderr=subprocess.STDOUT
-    )
-
-    es = Elasticsearch()
-    retries_left = 3
-    while True:
-        health = es.cluster.health()
-        if health.get("status") != "red":
-            break
-        if retries_left == 0:
-            terminate_process(process)
-            raise RuntimeError("Elasticsearch failed to start")
-        retries_left -= 1
-        sleep(10)
-
-    del es
-
-    yield "http://127.0.0.1:9201"
-
-    terminate_process(process)
+from pubmed_es.es_client import ElasticsearchClient
 
 
-def terminate_process(process):
-    process.terminate()
-    try:
-        process.wait(timeout=10)
-    except subprocess.TimeoutExpired:
-        process.kill()
+@pytest.mark.integration
+def test_es_client_parallel_bulk(elasticsearch_server):
+    ec = ElasticsearchClient()
+    ec.client.indices.create("foo")
+
+    def gendata():
+        mywords = ['foo', 'bar']
+        for i, word in enumerate(mywords):
+            yield {
+                "_index": "foo",
+                "_id": str(i),
+                "_type": "doc",
+                "doc": {"word": word},
+            }
+
+    ec.parallel_bulk(gendata)
+    assert ec.client.get(index="foo", id="0", doc_type="doc") == {"word": "foo"}
+    assert ec.client.get(index="foo", id="1", doc_type="doc") == {"word": "bar"}
